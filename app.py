f3e3b8c1f5045691f4da76a5404ba97b270ced46
harshvardhan <harshvardhanpurohit2020@gmail.com>
2024-04-11 17:56:50 +0530

    init

--- app.py
+++ app.py
@@ -55,6 +55,8 @@ submit1 = st.button("Tell Me About the Resume")
 #submit2 = st.button("How Can I Improvise my Skills")
 
 submit3 = st.button("Percentage match")
+submit2 = st.button("How can client improve its performance")
+
 
 input_prompt1 = """
  You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
@@ -63,9 +65,13 @@ input_prompt1 = """
 """
 
 input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of job filteration and ATS functionality, 
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
 the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+"""
+
+input_prompt2 = """
+You are an skilled consultant and tell me how the given candidate can improve its performance
 """
 
 if submit1:
@@ -75,7 +81,7 @@ if submit1:
         st.subheader("The Repsonse is")
         st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
 
 elif submit3:
     if uploaded_file is not None:
@@ -84,4 +90,13 @@ elif submit3:
         st.subheader("The Repsonse is")
         st.write(response)
     else:
-        st.write("Please uplaod the resume")
+        st.write("Please upload the resume")
+
+elif submit2:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt2,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
